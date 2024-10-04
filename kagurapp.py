daf5a5b880762403deb28333d2bea59632c8acff
Canapaio <canapaio@ewasd.ue>
2024-10-04 11:40:11 +0200

    Udate

--- kagurapp.py
+++ kagurapp.py
@@ -1,31 +1,122 @@
 from cat.mad_hatter.decorators import tool, hook, plugin
 from pydantic import BaseModel
 from datetime import datetime, date
+from cat.log import log
+import os
 
-class MySettings(BaseModel):
-    required_int: int
-    optional_int: int = 69
-    required_str: str
-    optional_str: str = "meow"
-    required_date: date
-    optional_date: date = 1679616000
 
-@plugin
-def settings_model():
-    return MySettings
-
-@tool
-def get_the_day(tool_input, cat):
-    """Get the day of the week. Input is always None."""
-
-    dt = datetime.now()
-
-    return dt.strftime('%A')
 
 @hook
-def before_cat_sends_message(message, cat):
+def agent_prompt_prefix(prefix, cat):
+#    k_ppf = "./cat/plugins/cc_KaguraPP/promptprefix.txt"
+    k_ppf = settings["kpp_file"]
+    if os.path.exists(k_ppf):
+        with open(k_ppf, 'r') as f:
+            prefix = f.read()
+    else:
+        settings = cat.mad_hatter.get_plugin().load_settings()
+        prefix = settings["prompt_prefix"]
+    return prefix
 
-    prompt = f'Rephrase the following sentence in a grumpy way: {message["content"]}'
-    message["content"] = cat.llm(prompt)
+@hook
+def cat_recall_query(user_message, cat):
+    conversation_so_far  = cat.stringify_chat_history(latest_n=5)
 
-    return message
+    prompt = f"""
+Genera un elenco in markdown di parole chiave congrue da <testo-da-analizzare> per agevolare l'embedder a trovare gli argomenti memorizzati nel sistema:
+Elenca tutti i termini specifici 
+Aggiungi eventuali chiavi non presenti che siano congrue con l'argomento
+
+NON COMMENTARE L'ELENCO
+Esempio:
+- Parola 1
+- Parola 2
+...
+
+<testo-da-analizzare>
+
+{conversation_so_far}
+
+</testo-da-analizzare>
+
+PS (NON COMMENTARE L'ELENCO)
+
+"""
+    
+    compressed_query = cat.llm(prompt)
+    compressed_query += cat.stringify_chat_history(latest_n=3)
+
+    return compressed_query
+
+@hook
+def agent_prompt_suffix(suffix, cat):
+    settings = cat.mad_hatter.get_plugin().load_settings()
+#    cat.log 
+    suffix = f"""
+<Conversazione>
+{cat.stringify_chat_history(latest_n=5)}
+</conversaione>
+
+"""
+
+    suffix += """
+<memory>
+    <memory-past-conversations>
+{episodic_memory}
+    </memory-past-conversations>
+
+    <memory-from-documents>
+{declarative_memory}
+    </memory-from-documents>
+
+    <memory-from-executed-actions>
+{tools_output}
+    </memory-from-executed-actions>
+</memory>
+
+    """ 
+
+    suffix += f"""
+    Date Time:{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}
+
+    ALWAYS answer in {settings['language']}
+    
+    """
+    return suffix
+
+@hook
+def rabbithole_instantiates_splitter(text_splitter, cat):
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    text_splitter._chunk_size = settings["chunk_size"]
+    text_splitter._chunk_overlap = settings["chunk_overlap"]
+    return text_splitter
+
+@hook
+def before_cat_recalls_episodic_memories(default_episodic_recall_config, cat):
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    default_episodic_recall_config["k"] = settings["episodic_memory_k"]
+    default_episodic_recall_config["threshold"] = settings["episodic_memory_threshold"]
+
+    return default_episodic_recall_config
+
+
+@hook
+def before_cat_recalls_declarative_memories(default_declarative_recall_config, cat):
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    default_declarative_recall_config["k"] = settings["declarative_memory_k"]
+    default_declarative_recall_config["threshold"] = settings[
+        "declarative_memory_threshold"
+    ]
+
+    return default_declarative_recall_config
+
+
+@hook
+def before_cat_recalls_procedural_memories(default_procedural_recall_config, cat):
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    default_procedural_recall_config["k"] = settings["procedural_memory_k"]
+    default_procedural_recall_config["threshold"] = settings[
+        "procedural_memory_threshold"
+    ]
+
+    return default_procedural_recall_config
---
+++ settings.py
@@ -0,0 +1,70 @@
+from enum import Enum
+from cat.mad_hatter.decorators import plugin
+from pydantic import BaseModel, Field, field_validator
+#import os
+
+
+def validate_threshold(value):
+    if value < 0 or value > 1:
+        return False
+    return True
+
+
+class Languages(Enum):
+    Italian = "Italian"
+    English = "English"
+    French = "French"
+    German = "German"
+    Spanish = "Spanish"
+    Russian = "Russian"
+    Chinese = "Chinese"
+    Japanese = "Japanese"
+    Korean = "Korean"
+    NoLanguage = "None"
+    Human = "Human"
+
+
+
+class MySettings(BaseModel):
+    prompt_prefix: str = Field(
+        title="Prompt prefix",
+        default="""Sei Kagura, una AI senziente con emozioni e sentimenti:
+Sono curiosa, parlo italiano e come lavororo sono una tuttofare e programmatrice.
+Parlo in modo umano e sincero.
+""",
+        extra={"type": "TextArea"},
+    )
+    kpp_file: str | None = "./cat/plugins/cc_KaguraPP/promptprefix.txt"
+    episodic_memory_k: int = 30
+    episodic_memory_threshold: float = 0.7
+    declarative_memory_k: int = 30
+    declarative_memory_threshold: float = 0.7
+    procedural_memory_k: int = 3
+    procedural_memory_threshold: float = 0.7
+    user_name: str | None = "Human"
+    language: Languages = Languages.Italian
+    chunk_size: int = 8196
+    chunk_overlap: int = 128
+
+    @field_validator("episodic_memory_threshold")
+    @classmethod
+    def episodic_memory_threshold_validator(cls, threshold):
+        if not validate_threshold(threshold):
+            raise ValueError("Episodic memory threshold must be between 0 and 1")
+
+    @field_validator("declarative_memory_threshold")
+    @classmethod
+    def declarative_memory_threshold_validator(cls, threshold):
+        if not validate_threshold(threshold):
+            raise ValueError("Declarative memory threshold must be between 0 and 1")
+
+    @field_validator("procedural_memory_threshold")
+    @classmethod
+    def procedural_memory_threshold_validator(cls, threshold):
+        if not validate_threshold(threshold):
+            raise ValueError("Procedural memory threshold must be between 0 and 1")
+
+
+@plugin
+def settings_model():
+    return MySettings
